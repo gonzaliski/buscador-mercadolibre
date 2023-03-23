34318518a2e73cf335fd2c2f4430099a03a932d0
Gon <gon.mendez13@gmail.com>
2023-03-23 01:18:24 -0300

    another wp change

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,10 +2,10 @@ import { Suspense } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import LoadingSpinner from "../loader/Loader";
 import { SearchForm } from "../searchForm/SearchForm";
-import meLiLogo from "../../assets/mercadolibre.svg";
-import reactLogo from "../../assets/react.svg";
-import recoilLogo from "../../assets/recoil-js.svg";
-import tsLogo from "../../assets/typescript.svg";
+import MeLiLogo from "../../assets/mercadolibre.svg";
+import ReactLogo from "../../assets/react.svg";
+import RecoilLogo from "../../assets/recoil-js.svg";
+import TsLogo from "../../assets/typescript.svg";
 import css from "./layout.css";
 function Layout() {
   let navigate = useNavigate();
@@ -18,12 +18,12 @@ function Layout() {
   return (
     <div>
       <header className={css["search__header"]}>
-        <img src={meLiLogo} className={css["logo"]} onClick={goHome}></img>
+        <MeLiLogo className={css["logo"]} onClick={goHome}></MeLiLogo>
         <SearchForm onSearch={handleSearch} />
         <div className={css["tools__container"]}>
-          <img src={reactLogo} className={css["logo__tool"]}></img>
-          <img src={recoilLogo} className={css["logo__tool"]}></img>
-          <img src={tsLogo} className={css["logo__tool"]}></img>
+          <ReactLogo className={css["logo__tool"]}></ReactLogo>
+          <RecoilLogo className={css["logo__tool"]}></RecoilLogo>
+          <TsLogo className={css["logo__tool"]}></TsLogo>
         </div>
       </header>
       <section className={css["content"]}>
